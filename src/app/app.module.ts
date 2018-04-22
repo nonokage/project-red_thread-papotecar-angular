d8e3ddb42e1f3879893633685958f6051e64ca02
nonokage <nononokage@gmail.com>
2018-04-22 17:25:38 +0200

    Creation du component modal pour la connexion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HeaderFormComponent } from './header-form/header-form.component';
 import { FooterComponent } from './footer/footer.component';
+import { ModalConnexionComponent } from './modal-connexion/modal-connexion.component';
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import { FooterComponent } from './footer/footer.component';
     AppComponent,
     NavbarComponent,
     HeaderFormComponent,
-    FooterComponent
+    FooterComponent,
+    ModalConnexionComponent
   ],
   imports: [
     BrowserModule

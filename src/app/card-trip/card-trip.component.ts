bcf2d717cb8f25a2291c037d8f49a74417b3f611
nonokage <nononokage@gmail.com>
2018-04-22 22:55:08 +0200

    Première modification du formulaire d'inscription

---
+++ src/app/card-trip/card-trip.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-trip',
+  templateUrl: './card-trip.component.html',
+  styleUrls: ['./card-trip.component.scss']
+})
+export class CardTripComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
